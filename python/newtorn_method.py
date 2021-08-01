2b709578ba18b84a96ef918cdcdfa671c727ccd9
l_vektor_m <lambdavektormuwork@gmail.com>
2021-08-01 08:27:00 +0000

    コーディング規約に則って

--- python/newtorn_method.py
+++ python/newtorn_method.py
@@ -9,8 +9,8 @@ epsilon = 1.0E-7  # Pと求めた平方根の2乗の差分の終了値
 print("計算開始")
 
 value1 = initial_value  # 計算ターンの初期値を入れる
-value2 = P_number/value1  # 計算ターンの初期値から計算した値を入れる
-avarage_value = (value1 + value2)/2.0  # 上記の平均値を取る
+value2 = P_number / value1  # 計算ターンの初期値から計算した値を入れる
+avarage_value = (value1 + value2) / 2.0  # 上記の平均値を取る
 
 for i in range(10**5):  # 最大計算回数は固定
     # 判定処理
@@ -21,7 +21,7 @@ for i in range(10**5):  # 最大計算回数は固定
 
     # 計算処理
     value1 = avarage_value
-    value2 = P_number/value1
-    avarage_value = (value1 + value2)/2.0
+    value2 = P_number / value1
+    avarage_value = (value1 + value2) / 2.0
 
 print("計算終了")
